23ad4273890eee9c4c4274a551d3a7841b3546d2
yakovenkoka <D00272517@student.dkit.ie>
2026-01-11 19:13:24 +0000

    feat: implement subscription logic

--- app/static/js/manage_events.js
+++ app/static/js/manage_events.js
@@ -4,10 +4,24 @@ async function fetchEvents() {
 
     if (response.ok) {
         data = await response.json()
-        console.log(data)
+        return data.events
     } else {
         console.error("Failed to fetch events:", response.status, response.statusText)
     }
+}
+
+async function addEvent(msg) {
+    url = "http://127.0.0.1:5001/events/save_event"
+
+    response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "description": msg
+        })
+    })
 }
 
 document.getElementById("load-events-btn").addEventListener("click", fetchEvents);
--- app/static/js/pubnub.js
+++ app/static/js/pubnub.js
@@ -1,3 +1,8 @@
+let sts = "OFF"
+let last_events = []
+let last_motion = null
+let motion = null
+
 const pubnub = new PubNub({
     publishKey: "pub-c-fc8a9a66-068b-49fe-a1e4-8f9ead36fcbc",
     subscribeKey: "sub-c-45f16351-7346-47d8-84d0-080414c6813c",
@@ -15,3 +20,39 @@ async function publishMessage(message) {
         console.error("Publish failed:", error);
     }
 }
+
+pubnub.subscribe({
+    channels: ["pir-events"]
+});
+
+pubnub.addListener({
+    message: async function (event) {
+        console.log("Raw PubNub event:", event);
+
+        const msg = event.message;
+
+        sts = msg.state;
+        console.log("Updated status to:", sts);
+
+        if (sts === "ON") {
+            await addEvent("Motion Detected");
+            console.log("Added 'Motion Detected' event");
+
+            last_events = await fetchEvents();
+            console.log("Fetched last events:", last_events);
+
+            last_motion = last_events.length > 0 ? last_events[last_events.length - 1].timestamp.$date : null;
+            console.log("Updated last motion to:", last_motion);
+
+        }
+
+        motion = sts === "ON" ? "Motion Detected" : "No Motion";
+        console.log("Updated motion to:", motion);
+    },
+
+    status: function (statusEvent) {
+        if (statusEvent.category === "PNConnectedCategory") {
+            console.log("Connected to PubNub channel");
+        }
+    }
+});
